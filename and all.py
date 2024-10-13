9fb3b2b513b101fdd02584fda4b84593f644adb3
dilara2016 <m.123.hunain@gmail.com>
2024-10-13 11:29:50 +0200

    and all

---
+++ and all.py
@@ -0,0 +1,20 @@
+a = 10
+b = 12
+c = 0
+if a and b and c:
+ print("ALL the numbers have boolean value as True")
+else:
+  print("Atleast one numbers have boolean value as False")
+
+
+a = 10
+b = -10
+c = 0
+if a > 0 or b > 0:
+  print("Either of the numbers is greater than 0")
+else:
+ print("no number is greater than 0")
+if b > 0 or c > 0:
+  print("Either of the numbers is greater than 0")
+else:
+ print("no number is greater than 0")
